1066f553554b519b3c88c13af6ae226bb2a2effa
Shubham Jalan <92722931+Shubhamjalan01@users.noreply.github.com>
2023-02-04 12:55:34 +0530

    Add files via upload

--- demo.py
+++ demo.py
@@ -12,8 +12,9 @@ data = {
         }
     }
 }
-#data['<sequence_id>'] = data[10]
-data['access_lists']['<access_list_name>']['sequence_numbers']['<sequence_id>']['action'] = 'ip any'
-data['access_lists']['<access_list_name>']['counters_per_entry'] = 'true'
+data['access_lists']['<access_list_name>']['sequence_numbers']['<sequence_id>']['action'] = 'permit ip 10.10.10.0/24 any'
+data['access_lists']['<access_list_name>']['counters_per_entry'] = True
+data['access_lists']['<access_list_name>']['sequence_numbers'][10]=data['access_lists']['<access_list_name>']['sequence_numbers'].pop('<sequence_id>')
+data['access_lists']['test1'] = data['access_lists'].pop('<access_list_name>')
 with open('test.txt', mode='w') as f:
     yaml.dump(data, f, indent=2)
