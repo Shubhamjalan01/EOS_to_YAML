69b0962e84cbf08a926d504654f9cf55cddc17a8
Shubham Jalan <92722931+Shubhamjalan01@users.noreply.github.com>
2023-02-04 17:37:58 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,17 +1,6 @@
 import yaml
 
-data = {
-    'access_lists': {
-        'test1': {
-            'counters_per_entry': '<true | false>',
-            'sequence_numbers': {
-                '<sequence_id>': {
-                    'action': "<action_string>"
-                }
-            }
-        }
-    }
-}
+data = {}
 mylines = []                             
 with open ('input.txt', 'rt') as myfile: 
     for myline in myfile:                
@@ -21,12 +10,21 @@ mylines.pop(0)
 print(mylines)
 seq = [i.split()[0] for i in mylines]
 print(seq)
+act = [i.partition(' ')[2] for i in mylines]
+act1 = [i.split(' ',1)[1] for i in act]
+act3 = []
+for s in act1:
+    act3.append(s.replace("\n", " "))
+print(act3)
 f = open('output.txt', mode='w')
 for x in seq:
-    data['access_lists']['test1']['sequence_numbers'] = {
+    data['access_lists'] = {'test1': {
+            'counters_per_entry': 'true',
+            'sequence_numbers': {
                 x: {
-                    'action': "permit ip 10.10.10.0/24 host 10.20.10.1"
+                    'action': act3[0]
                 }
             }
+        }
+    }
     yaml.dump(data, f, indent=2)
-f.close()
