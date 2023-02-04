0a1867472f0632f86814010327dbc5158e3e4370
Shubham Jalan <92722931+Shubhamjalan01@users.noreply.github.com>
2023-02-04 14:14:11 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -10,17 +10,15 @@ mylines.pop(0)
 print(mylines)
 seq = [i.split()[0] for i in mylines]
 print(seq)
-data['access_lists'] = {'test1': {
+for x in seq:
+    data['access_lists'] = {'test1': {
             'counters_per_entry': 'true',
             'sequence_numbers': {
-                seq[0]: {
+                x: {
                     'action': "permit ip 10.10.10.0/24 host 10.20.10.1"
-                },
-                seq[1]: {
-                    'action': "ip"
                 }
             }
         }
-}
+    }
 with open('output.txt', mode='w') as f:
     yaml.dump(data, f, indent=2)
