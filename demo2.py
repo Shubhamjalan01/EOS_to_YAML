b2c7f7afa3e831278d47d6c7e63223042705939a
Shubham Jalan <92722931+Shubhamjalan01@users.noreply.github.com>
2023-02-04 13:58:55 +0530

    Add files via upload

--- demo2.py
+++ demo2.py
@@ -1,14 +1,23 @@
 import yaml
 
 data = {}
+mylines = []                             
+with open ('input.txt', 'rt') as myfile: 
+    for myline in myfile:                
+        mylines.append(myline)           
+print(mylines)
+mylines.pop(0)
+print(mylines)
+seq = [i.split()[0] for i in mylines]
+print(seq)
 data['access_lists'] = {'test1': {'counters_per_entry': 'true','sequence_numbers': {10: {'action': "permit ip 10.10.10.0/24 any"}}}}
 data['access_lists']['test2'] = {
             'counters_per_entry': 'true',
             'sequence_numbers': {
-                10: {
+                seq[0]: {
                     'action': "permit ip 10.10.10.0/24 host 10.20.10.1"
                 },
-                20: {
+                seq[1]: {
                     'action': "ip"
                 }
             }
