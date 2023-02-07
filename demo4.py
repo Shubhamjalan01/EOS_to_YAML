283456f8e49c2da16620bbfe5efb42b1f9513f24
Shubham Jalan <92722931+Shubhamjalan01@users.noreply.github.com>
2023-02-07 21:05:30 +0530

    Add files via upload

---
+++ demo4.py
@@ -0,0 +1,45 @@
+import yaml
+
+res = {}    
+alnames = []  
+#iterate over the input file                      
+with open ('input.txt', 'rt') as myfile: 
+    for myline in myfile:
+        #create a empty temporary dict.
+        temp = {}
+        if "Access List" in myline:
+            #Extract access list name from config
+            lis = myline.split()
+            alnames.append(lis[-1])
+            print(lis[-1])
+            #create a temporary dictionary with given accesslist name.
+            #temp['access_lists'][lis[-1]] = temp['access_lists'].pop('<access_list_name>')
+            temp['access_lists'] = {lis[-1]: {
+            'counters_per_entry': 'true',
+            'sequence_numbers': {
+                }
+            }}
+            
+            print(temp)
+            #update temp dictionary to res dictionary
+            if len(alnames)>1:
+                #res.update(temp['access_lists'])
+                #merge new access list to the existing access list
+                res['access_lists'] = res['access_lists']|temp['access_lists']
+            else:
+                res.update(temp)
+            print(res)
+            print(alnames)
+        else:
+            #add values to the dictionary for different sequence numbers
+            #for x in alnames:
+            var = myline.strip('\n')
+            res['access_lists'][alnames[-1]]['sequence_numbers'][myline[1:3]] =  {
+                    'action': var[4:]
+                }
+            print(res)
+print(res)   
+# write dictionary to file converting into yaml format  
+with open('output.txt', mode='w') as f:
+    yaml.dump(res, f, indent=2)
+f.close()
